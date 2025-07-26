2d4b15654227906eb60a0b92c8b3bd91c6a6349f
faheemcp <faheemcp241@gmail.com>
2025-07-26 17:30:02 +0530

    Final try

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -1,33 +1,9 @@
-import { useToast } from "@/hooks/use-toast"
-import {
-  Toast,
-  ToastClose,
-  ToastDescription,
-  ToastProvider,
-  ToastTitle,
-  ToastViewport,
-} from "@/components/ui/toast"
+import { useEffect } from "react";
 
 export function Toaster() {
-  const { toasts } = useToast()
+  useEffect(() => {
+    alert("Toast initialized!");
+  }, []);
 
-  return (
-    <ToastProvider>
-      {toasts.map(function ({ id, title, description, action, ...props }) {
-        return (
-          <Toast key={id} {...props}>
-            <div className="grid gap-1">
-              {title && <ToastTitle>{title}</ToastTitle>}
-              {description && (
-                <ToastDescription>{description}</ToastDescription>
-              )}
-            </div>
-            {action}
-            <ToastClose />
-          </Toast>
-        )
-      })}
-      <ToastViewport />
-    </ToastProvider>
-  )
+  return null; // Or return styled toast component here
 }
